44361f66c93c2c4ef29e68082389e096f39fde10
park91s <park91s@naver.com>
2024-06-12 23:59:11 +0900

    [DDL][추가][ALTER문을 생성하는 기능을 추가]

--- src/views/project/ddl/DdlView.vue
+++ src/views/project/ddl/DdlView.vue
@@ -2,37 +2,54 @@
   <div class="container">
 	<CommonHeader :menuName="initdata.menuName" :add :get :del :save/>
 	<div class="row">
-		<div class="col-10">
-			<label for="projectCodeSel">프로젝트 코드</label>
-			<select id="projectCodeSel" name="projectCodeSel">
+		<div class="col input-group mb-3">
+			<label for="projectCodeSel" class="input-group-text">프로젝트 코드</label>
+			<select id="projectCodeSel" name="projectCodeSel" class="form-select form-select-sm">
 			</select>
 		</div>
-		<div class="col-2">
+		<div class="col input-group mb-3">
+			<label for="tableNameSel" class="input-group-text">테이블 명</label>
+			<select id="tableNameSel" name="tableNameSel" class="form-select form-select-sm">
+			</select>
+		</div>
+		<div class="col input-group mb-3">
+			<label for="tableColSel" class="input-group-text">테이블 컬럼</label>
+			<select id="tableColSel" name="tableColSel" class="form-select form-select-sm">
+			</select>
+		</div>
+		<div class="col right-box">
 			<button :onclick="script">
-				스크립트 보기
+				테이블 추가
+			</button>
+			<button :onclick="alterAddScript">
+				컬럼 추가
+			</button>
+			<button :onclick="alterModifyScript">
+				컬럼 변경
 			</button>
 		</div>
 	</div>
 	<br>
 	<BaseTable :id="gridId">
 	</BaseTable>
-  </div>
-  <div id="scriptModalId" class="modal">
-	  <div class="modal-content animate" style="height: 80vh;">
+</div>
+<div id="scriptModalId" class="modal">
+	<div class="modal-content animate" style="height: 80vh;">
 		<span v-on:click="hideModal" class="close" title="Close Modal">&times;</span>
 		<div>
 			<label>스크립트 결과</label>
 		</div>
 		<div>
-			<select id="dbType" style="width: 20vw;" :onchange="script">
+			<select id="dbType" style="width: 20vw; margin: 8px;" :onchange="script">
 				<option value="mysql">mysql</option>
 				<option value="mariadb">mariadb</option>
 				<option value="oracle">oracle</option>
 				<option value="postgres">postgres</option>
 				<option value="mssql">mssql</option>
 			</select>
+			<button v-on:click="copyScript">Copy</button>
 		</div>
-		<textarea id="scriptResult" style="height: 100vh;"></textarea>
+		<textarea id="scriptResult" style="height: 100vh;" disabled></textarea>
 	</div>
   </div>
 </template>
@@ -42,7 +59,7 @@
 
 <script setup lang="ts">
 import { isNotEmpty, isEmpty, getId} from '@/utils/common'
-import { ALIGN, DataType, GRID_MSG, HeaderInfo, HeaderInfoBuilder, OptionInfo, TableBuilder } from '@/utils/tableBuilder';
+import { ALIGN, DataType, GRID_MSG, HeaderInfo, HeaderInfoBuilder, OptionInfo, TableBuilder, SORT, DATA_CLASS} from '@/utils/tableBuilder';
 import { onMounted } from 'vue';
 import CommonHeader from '@/components/common/header/CommonHeader.vue';
 import BaseTable from '@/components/table/BaseTable.vue';
@@ -74,6 +91,12 @@ const initdata = {
 		type: '',
 		media: '',
 		rel: ''
+	},
+	alterScript: {
+		href: '',
+		type: '',
+		media: '',
+		rel: ''
 	}
 }
 
@@ -85,36 +108,57 @@ const getApi = () =>{
 	http.contentTypeJson();
 	http.setToken(store.storeToken);
 	http.setSuccess(function(res: any){
-		console.log(res);
-		initdata.menuName = res.data.menuName;
-		initdata.list = res.data.list;
-		initdata.save = res.data.save;
-		initdata.script = res.data.script;
+		const data = res.data;
+
+		//Link
+		initdata.menuName = data.menuName;
+		initdata.list = data.list;
+		initdata.save = data.save;
+		initdata.script = data.script;
+		initdata.alterScript = data.alterScript;
+
+		//selectBox
+		[
+			{id: 'projectCodeSel', data: data.projectCodeSel},
+			{id: 'tableNameSel', data: data.tableNameSel},
+			{id: 'tableColSel', data: data.tableColSel},
+		].forEach(( selData )=>{
+			const sel = (getId(selData.id) as HTMLSelectElement);
+			sel.innerHTML = '';
+			selData.data.forEach((option: string)=>{
+				const opt = new OptionInfo();
+				opt.text = option;
+				opt.value = option;
+				sel.appendChild(opt.createOption());
+			});
+		})
 	});
+
+	(getId('searchTxt') as HTMLInputElement).onkeyup = (e: KeyboardEvent) =>{
+		if(e.code === 'Enter' || e.code === 'NumpadEnter'){
+			get();
+		}
+	}
 
 	callAPI(http);
 }
 
 const get = () =>{
 	let grid = initdata.grid;
+	let url = initdata.list.href;
+	url += "?projectCode="+(getId('projectCodeSel') as HTMLSelectElement).value;
+	url += "&tableName="+(getId('tableNameSel') as HTMLSelectElement).value;
+	url += "&tableCol="+(getId('tableColSel') as HTMLSelectElement).value;
+	url += "&searchTxt="+(getId('searchTxt') as HTMLInputElement).value;
+
 	let http = new Http();
 	http.setParam(null);
-	http.setUrl(initdata.list.href+"?projectCode="+(getId('projectCodeSel') as HTMLSelectElement).value);
+	http.setUrl(url);
 	http.method(initdata.list.type);
 	http.setContentType(initdata.list.media);
 	http.setToken(store.storeToken);
 	http.setSuccess(function(res: any){
 		const data = res.data;
-		console.log(data);
-		const selBox = (getId('projectCodeSel') as HTMLSelectElement);
-		selBox.innerHTML='';
-		data.selectBox.forEach((option : string) => {
-			const opt = new OptionInfo();
-			opt.text = option;
-			opt.value = option;
-			selBox.appendChild(opt.createOption());
-		});
-		
 
 		grid.clear();
 		grid.remakeFromJSONListToData(data.list);
@@ -143,31 +187,33 @@ const buildGrid = () => {
 	let grid = initdata.grid;
 
 	const headerInfo =[
-		['no','','상태','프로젝트코드','테이블명','테이블컬럼','컬럼 타입','컬럼 타입 제한','필수여부', 'PK 여부', '기본값', '설명', '정렬 순번']
+		['no','','상태','프로젝트코드','테이블명','테이블컬럼','컬럼 타입','컬럼 타입 제한','NULL 여부', 'PK 여부', '기본값', '설명', '정렬 순번']
 	];
 	
 	let headers: HeaderInfo[] = [
-		{colId: 'no', width: '50px', dtType: DataType.READ_ONLY, align: ALIGN.CENTER},
-		{colId: 'chkbox', width: '20px', dtType: DataType.CHECK, align: ALIGN.CENTER},
-		{colId: 'status', width: '50px', dtType: DataType.READ_ONLY, align: ALIGN.CENTER},
-		{colId: 'projectCode', width: '80px', dtType: DataType.TEXT, align: ALIGN.LEFT},
-		{colId: 'tableName', width: '80px', dtType: DataType.TEXT, align: ALIGN.LEFT},
-		{colId: 'tableCol', width: '80px', dtType: DataType.TEXT, align: ALIGN.LEFT},
-		{colId: 'colType', width: '80px', dtType: DataType.SELECT, align: ALIGN.LEFT},
-		{colId: 'colTypeLmt', width: '100px', dtType: DataType.TEXT, align: ALIGN.RIGHT},
-		{colId: 'nullable', width: '80px', dtType: DataType.SELECT, align: ALIGN.LEFT},
-		{colId: 'colPk', width: '80px', dtType: DataType.SELECT, align: ALIGN.LEFT},
-		{colId: 'defaultValue', width: '80px', dtType: DataType.TEXT, align: ALIGN.LEFT},
-		{colId: 'comment', width: '80px', dtType: DataType.TEXT, align: ALIGN.LEFT},
-		{colId: 'sortSeq', width: '80px', dtType: DataType.TEXT, align: ALIGN.RIGHT},
+		{colId: 'no', width: '50px', dtType: DataType.READ_ONLY, align: ALIGN.CENTER, sort: SORT.DESC, dataClass: DATA_CLASS.NUMBER},
+		{colId: 'chkbox', width: '20px', dtType: DataType.CHECK, align: ALIGN.CENTER, sort: SORT.NONE, dataClass: DATA_CLASS.NONE},
+		{colId: 'status', width: '50px', dtType: DataType.READ_ONLY, align: ALIGN.CENTER, sort: SORT.ASC, dataClass: DATA_CLASS.STRING},
+		{colId: 'projectCode', width: '80px', dtType: DataType.TEXT, align: ALIGN.LEFT, sort: SORT.ASC, dataClass: DATA_CLASS.STRING},
+		{colId: 'tableName', width: '80px', dtType: DataType.TEXT, align: ALIGN.LEFT, sort: SORT.ASC, dataClass: DATA_CLASS.STRING},
+		{colId: 'tableCol', width: '80px', dtType: DataType.TEXT, align: ALIGN.LEFT, sort: SORT.ASC, dataClass: DATA_CLASS.STRING},
+		{colId: 'colType', width: '80px', dtType: DataType.SELECT, align: ALIGN.LEFT, sort: SORT.ASC, dataClass: DATA_CLASS.STRING},
+		{colId: 'colTypeLmt', width: '100px', dtType: DataType.TEXT, align: ALIGN.RIGHT, sort: SORT.NONE, dataClass: DATA_CLASS.STRING},
+		{colId: 'nullable', width: '80px', dtType: DataType.SELECT, align: ALIGN.LEFT, sort: SORT.ASC, dataClass: DATA_CLASS.STRING},
+		{colId: 'colPk', width: '80px', dtType: DataType.SELECT, align: ALIGN.LEFT, sort: SORT.ASC, dataClass: DATA_CLASS.STRING},
+		{colId: 'defaultValue', width: '80px', dtType: DataType.TEXT, align: ALIGN.LEFT, sort: SORT.ASC, dataClass: DATA_CLASS.STRING},
+		{colId: 'comment', width: '80px', dtType: DataType.TEXT, align: ALIGN.LEFT, sort: SORT.ASC, dataClass: DATA_CLASS.STRING},
+		{colId: 'sortSeq', width: '80px', dtType: DataType.TEXT, align: ALIGN.RIGHT, sort: SORT.ASC, dataClass: DATA_CLASS.NUMBER},
 	].map((item, colIdx)=>{
 		return new HeaderInfoBuilder()
-					.setColId(item.colId)
-					.setWidth(item.width)
-					.setDateType(item.dtType)
-					.setColIdx(colIdx)
-					.setAlign(item.align)
-					.build()
+				.setColId(item.colId)
+				.setWidth(item.width)
+				.setDateType(item.dtType)
+				.setColIdx(colIdx)
+				.setAlign(item.align)
+				.setSort(item.sort)
+				.setDataClass(item.dataClass)
+				.build();
 	});
 
 	grid.setHeadText(headerInfo);
@@ -203,6 +249,7 @@ const add = () => {
 	param['status']	 		= GRID_MSG.INSERT;
 	param['projectCode'] 	= '';
 	param['tableName'] 		= '';
+	param['tableCol'] 		= '';
 	param['colType'] 		= '';
 	param['colTypeLmt'] 	= '';
 	param['nullable'] 		= '0';
@@ -223,9 +270,11 @@ const del = () => {
 			initdata.grid.setCellValueById(rowIdx,'status',GRID_MSG.DELETE);
 			initdata.grid.setCellValueById(rowIdx,'chkbox',"0");
 			(initdata.grid.getCellChildById(rowIdx,'chkbox') as HTMLInputElement).checked = false;
+
 			let isDisabled = initdata.pks.every((colId) => {
 				return !initdata.grid.isDisabledCellById(rowIdx, colId);
 			});
+
 			if(isDisabled){
 				deleteRows.push(rowIdx);
 			}
@@ -263,7 +312,7 @@ const save = () => {
 		
 		if(param.addList.length == 0 
 				&& param.deleteList.length == 0 
-				&& param.updateList == 0){
+				&& param.updateList.length == 0){
 			alert("변경사항이 없습니다.");
 			valid = false;
 		}else{
@@ -297,7 +346,8 @@ const save = () => {
 		let http = new Http();
 		http.setParam(param);
 		http.setUrl(initdata.save.href);
-		http.setContentType(initdata.save.media);
+		//http.setContentType(initdata.save.media);
+		http.contentTypeJson();
 		http.method(initdata.save.type);
 		http.setToken(store.storeToken);
 		http.setSuccess(function(){
@@ -315,6 +365,7 @@ const scriptModalId = 'scriptModalId';
 const script = () =>{
 	let url = initdata.script.href;
 	url += "?projectCode="+ (getId('projectCodeSel') as HTMLSelectElement).value;
+	url += "&tableName="+ (getId('tableNameSel') as HTMLSelectElement).value;
 	url += "&dbType="+(getId('dbType') as HTMLSelectElement).value;
 
 	let http = new Http();
@@ -325,7 +376,52 @@ const script = () =>{
 	http.setToken(store.storeToken);
 	http.setSuccess(function(res: any){
 		const data = res.data;
-		console.log(data.script);
+		const scriptResult = getId('scriptResult') as HTMLTextAreaElement;
+		scriptResult.textContent = data.script;
+	});
+
+	callAPI(http);
+	showModal();
+}
+
+const alterAddScript = () =>{
+	let url = initdata.alterScript.href;
+	url += "?projectCode="+ (getId('projectCodeSel') as HTMLSelectElement).value;
+	url += "&tableName="+ (getId('tableNameSel') as HTMLSelectElement).value;
+	url += "&dbType="+(getId('dbType') as HTMLSelectElement).value;
+	url += "&alterType=ADD";
+
+	let http = new Http();
+	http.setParam(null);
+	http.setUrl(url);
+	http.method(initdata.alterScript.type);
+	http.setContentType(initdata.alterScript.media);
+	http.setToken(store.storeToken);
+	http.setSuccess(function(res: any){
+		const data = res.data;
+		const scriptResult = getId('scriptResult') as HTMLTextAreaElement;
+		scriptResult.textContent = data.script;
+	});
+
+	callAPI(http);
+	showModal();
+}
+
+const alterModifyScript = () =>{
+	let url = initdata.alterScript.href;
+	url += "?projectCode="+ (getId('projectCodeSel') as HTMLSelectElement).value;
+	url += "&tableName="+ (getId('tableNameSel') as HTMLSelectElement).value;
+	url += "&dbType="+(getId('dbType') as HTMLSelectElement).value;
+	url += "&alterType=MODIFY";
+
+	let http = new Http();
+	http.setParam(null);
+	http.setUrl(url);
+	http.method(initdata.alterScript.type);
+	http.setContentType(initdata.alterScript.media);
+	http.setToken(store.storeToken);
+	http.setSuccess(function(res: any){
+		const data = res.data;
 		const scriptResult = getId('scriptResult') as HTMLTextAreaElement;
 		scriptResult.textContent = data.script;
 	});
@@ -340,6 +436,11 @@ const showModal = () => {
 
 const hideModal = () => {
 	(document.getElementById(scriptModalId) as HTMLDivElement).style.display="none";
+}
+
+const copyScript = () => {
+	var copyText = (getId('scriptResult') as HTMLTextAreaElement);
+	navigator.clipboard.writeText(copyText.value);
 }
 
 onMounted(()=>{
